83165f4808d82c011c8c093103d97f13dcb6d3c3
Lukáš Orčík <41975205+LukasOrcik@users.noreply.github.com>
2022-11-13 22:18:39 +0000

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,5 @@
-#!/bin/bash
-set -euo pipefail
+#!/bin/sh -l
 
-install_zip_dependencies(){
-	echo "Installing and zipping dependencies..."
-	mkdir python
-	pip install --target=python -r "${INPUT_REQUIREMENTS_TXT}"
-	zip -r dependencies.zip ./python
-}
-
-publish_dependencies_as_layer(){
-	echo "Publishing dependencies as a layer..."
-	local result=$(aws lambda publish-layer-version --layer-name "${INPUT_LAMBDA_LAYER_ARN}" --zip-file fileb://dependencies.zip)
-	LAYER_VERSION=$(jq '.Version' <<< "$result")
-	rm -rf python
-	rm dependencies.zip
-}
-
-
-deploy_lambda_function(){
-	install_zip_dependencies
-	publish_dependencies_as_layer
-}
-
-deploy_lambda_function
-echo "Done."
+echo "Hello $1"
+time=$(date)
+echo "time=$time" >> $GITHUB_OUTPUT
