19941c49695e9783133af1b65aeddf8618d3e13f
Lukáš Orčík <41975205+LukasOrcik@users.noreply.github.com>
2022-11-14 22:46:49 +0000

    update cp

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-install_zip_dependencies(){
+install_zip_dependencies()
+{
 	echo "Installing and zipping dependencies..."
 	mkdir python
 	pip install --target=python -r "${INPUT_REQUIREMENTS_TXT}" --no-cache-dir
@@ -10,9 +11,16 @@ install_zip_dependencies(){
 	echo "Done installing and zipping dependencies"
 }
 
-publish_dependencies_as_layer(){
+publish_dependencies_as_layer()
+{
 	echo "Publishing dependencies as a layer..."
-	local result=$(aws lambda publish-layer-version --layer-name ${INPUT_LAMBDA_LAYER_ARN} --compatible-architectures ${INPUT_ARCHITECTURES} --compatible-runtimes ${INPUT_RUNTIMES} --zip-file fileb://dependencies.zip)
+	local result="$(aws lambda publish-layer-version					\
+					--layer-name ${INPUT_LAMBDA_LAYER_ARN}				\
+					--compatible-architectures ${INPUT_ARCHITECTURES}	\
+					--compatible-runtimes ${INPUT_RUNTIMES}				\
+					--description ${INPUT_LAYER_DESCRIPTION}			\
+					--zip-file fileb://dependencies.zip					\
+					)"
 	LAYER_VERSION=$(jq '.Version' <<< "$result")
 	rm -rf python
 	rm dependencies.zip
@@ -20,7 +28,8 @@ publish_dependencies_as_layer(){
 }
 
 
-deploy_lambda_layer(){
+deploy_lambda_layer()
+{
 	install_zip_dependencies
 	publish_dependencies_as_layer
 }
