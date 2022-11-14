dd4bd860903f78533a69df62353347279faf26cf
Lukáš Orčík <41975205+LukasOrcik@users.noreply.github.com>
2022-11-14 10:27:08 +0000

    update cp

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 install_zip_dependencies(){
 	echo "Installing and zipping dependencies..."
 	mkdir python
-	pip install --target=python -r "${INPUT_REQUIREMENTS_TXT}"
-	cp -r $INPUT_PACKAGE_PATH python/
+	# pip install --target=python -r "${INPUT_REQUIREMENTS_TXT}"
+	cp -r ${INPUT_PACKAGE_PATH} python/
 	# cp -r common/* python/
 	echo $(ls)
 	zip -r dependencies.zip ./python
